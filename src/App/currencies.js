def00854c5f56098c58eb51f0c56a64c69112700
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-08-25 17:45:07 +0200

    create app components

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "U.S. Dollar",
+        rate: 4.4278,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.9384,
+    },
+    {
+        short: "GBP",
+        name: "British Pound",
+        rate: 3.4278,
+    },
+]
