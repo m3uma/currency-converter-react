f92a693e1d0919bd6134524d3cd4ea68cf296f67
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-08-26 18:57:20 +0200

    fixed background

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import background from './background.jpg'
 
 export const GlobalStyle = createGlobalStyle`
     html {
@@ -15,7 +16,7 @@ export const GlobalStyle = createGlobalStyle`
         align-items: center;
         justify-content: center;
         min-height: 100vh;
-        background-image: url("background.jpg");
+        background-image: url("${background}");
         background-size: cover;
         background-position: center;
     }
