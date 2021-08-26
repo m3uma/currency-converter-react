f67db629be9fbae1e8bd519f3a123fb04ab834ec
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-08-26 18:23:42 +0200

    adding theme and GlobalStyle

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        black: "#000",
+        emperor:"#555",
+        boulder:"#777",
+        white: "#fff",
+        brown: "#994500",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
